97fa332b9abdf2754d575228be543d4c5efdfdb4
Peter Iedema <piedema@gmail.com>
2023-01-10 15:37:18 +0100

    edited script

--- scripts/default/db-init-with-data.sh
+++ scripts/default/db-init-with-data.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-cd ../../api/default/scripts && node db-init-with-data.js
+cd ../../api/custom/scripts && node db-init-with-data.js || cd ../../api/default/scripts && node db-init-with-data.js
 read -p "Press any key to continue . . ."
